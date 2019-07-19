b7a8a8aa46d2dd8ff07606baeb69afd300c68bf3
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2019-07-19 19:44:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function sizeCheck(x){
 
 // DropDown Menu --------------------------------------------
 var dropContent = document.querySelector('.dropdown-content');
-dropContent.style.left = s[0] - 220 +"px"; //for printing for the first time
+dropContent.style.left = s[0] - 240 +"px"; //for printing for the first time
 function dropDown(x){
 	if(x >= 540){
 		if(s[0] != x){
-			dropContent.style.left = x - 220 +"px";
+			dropContent.style.left = x - 240 +"px";
 		}
 	}
 }
@@ -62,6 +62,4 @@ hamMenu.addEventListener("click",function(){
 });
 
 // Events -------------------------------------------------
-var eventList = document.querySelector('#events li');
 
-eventList.forEach((listItem) => { listItem.addEventListener("click",function(){ listItem.classList.toggle('details'); }) });
